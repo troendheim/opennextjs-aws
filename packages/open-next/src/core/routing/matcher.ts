b16946f0bc56571304cf8b543b956380b7cab85a
Richard Trøndheim <rtr@sport24.dk>
2026-05-18 13:49:03 +0200

    fix: restore x-nextjs-data header on data requests (#1170)

--- packages/open-next/src/core/routing/matcher.ts
+++ packages/open-next/src/core/routing/matcher.ts
@@ -411,6 +411,7 @@ export function fixDataPage(
       ...internalEvent,
       rawPath: newPath,
       query,
+      headers: { ...internalEvent.headers, "x-nextjs-data": "1" },
       url: new URL(
         `${newPath}${convertToQueryString(query)}`,
         internalEvent.url,
